f86b712b40a2fd7a34013b84a3f2ad6c8454ebb4
이로움 <JK1234~~>
2024-11-04 21:20:02 +0900

    Add albamon_FE initial settings [main]

---
+++ albamon_FE/pages/index.tsx
@@ -0,0 +1,13 @@
+// /pages/index.tsx
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Albamon_SD_UI</h1>
+            <p>This is the homepage.</p>
+        </div>
+    );
+};
+
+export default Home;
