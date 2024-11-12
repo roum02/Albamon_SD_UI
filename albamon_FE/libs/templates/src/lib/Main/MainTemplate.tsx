36fe88f0eef02ccb951ef59f0699d1fa26a4bcca
이로움 <JK1234~~>
2024-11-12 16:51:28 +0900

    Remove test api uri [feature/react-query]

--- albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
@@ -8,79 +8,35 @@ interface NextRequestInit extends RequestInit {
   };
 }
 
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
+const GET_CARDS = gql`
+  query GetCards {
+      getCards {
+        resultCode
+        resultMessage
+        totalCount
+        row
+        column
+        collection {
+          franchiseCode
+          franchiseName
+          franchiseLogo
+          keywordCode
+          keywordName
+          partCode
+          partName
+        }
+      }
     }
-  }
 `;
 
 
 export const MainTemplate = () => {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
-    console.log("hello: ", data)
+    const { loading, error, data } = useQuery(GET_CARDS);
+
+    console.log(data)
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
-
-  // client
-  //     .query({
-  //       query: gql`
-  //     query GetLocations {
-  //       locations {
-  //         id
-  //         name
-  //         description
-  //         photo
-  //       }
-  //     }
-  //   `,
-  //     })
-  //     .then((result) => console.log(result));
-
-  // const fetchCards = async () => {
-  //   const query = `
-  //   query GetCards {
-  //     getCards {
-  //       resultCode
-  //       resultMessage
-  //       totalCount
-  //       row
-  //       column
-  //       collection {
-  //         franchiseCode
-  //         franchiseName
-  //         franchiseLogo
-  //         keywordCode
-  //         keywordName
-  //         partCode
-  //         partName
-  //       }
-  //     }
-  //   }
-  // `;
-  //   const response = await fetch('https://albamon-sd-ui.onrender.com/', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ query }),
-  //     next: { revalidate: 10 },
-  //   } as NextRequestInit);
-  //
-  //
-  //   const data = await response.json();
-  //   return data;
-  // };
-  //
-  // // Example usage
-  // fetchCards()
-  //   .then((data) => console.log(data))
-  //   .catch((error) => console.error('Error fetching cards:', error));
 
   return (
     <div>
