abbccf6e7d0057aaaa5877c1821b6ccea4546540
이로움 <JK1234~~>
2024-11-12 17:40:37 +0900

    Add options [feature/react-query]

--- albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
@@ -1,6 +1,7 @@
 import { MainBanner } from '@components/lib';
 import { gql, useQuery } from "@apollo/client";
 import { client } from '@apollo-client/client'
+import { NetworkStatus } from '@apollo/client';
 
 
 interface NextRequestInit extends RequestInit {
@@ -33,17 +34,24 @@ const GET_CARDS = gql`
 
 export const MainTemplate = () => {
     // client version
-    const { loading, error, data } = useQuery(GET_CARDS);
+    const { loading, error, data, networkStatus, refetch } = useQuery(GET_CARDS, {
+        //pollInterval: 500,
+        notifyOnNetworkStatusChange: true,
+    });
 
     console.log(data)
+    if (networkStatus === NetworkStatus.refetch) return <p>Refetching!</p>;
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
   return (
-    <div>
-      <MainBanner />
-      <h1>main card list</h1>
-    </div>
+      <div>
+          <MainBanner/>
+          <button onClick={() => refetch()}>
+              Refetch
+          </button>
+          <h1>main card list</h1>
+      </div>
   );
 };
 
