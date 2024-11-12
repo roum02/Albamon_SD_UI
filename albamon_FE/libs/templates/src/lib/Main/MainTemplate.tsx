2e9329351f3c3784e93c3c15b7ebbbad0e10d95e
이로움 <JK1234~~>
2024-11-12 14:33:14 +0900

    Add fetch query [main]

--- albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
@@ -1,6 +1,53 @@
 import { MainBanner } from "@components/lib";
 
-export const MainTemplate = () => {
+interface NextRequestInit extends RequestInit {
+    next?: {
+        revalidate: number;
+    };
+}
+
+
+export const MainTemplate =  () => {
+    const fetchCards = async () => {
+        const query = `
+    query GetCards {
+      getCards {
+        resultCode
+        resultMessage
+        totalCount
+        row
+        column
+        collection {
+          franchiseCode
+          franchiseName
+          franchiseLogo
+          keywordCode
+          keywordName
+          partCode
+          partName
+        }
+      }
+    }
+  `;
+        const response = await fetch('https://albamon-sd-ui.onrender.com/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query }),
+            next: { revalidate: 10 },
+        } as NextRequestInit);
+
+        const data = await response.json();
+        return data;
+    };
+
+// Example usage
+    fetchCards()
+        .then(data => console.log(data))
+        .catch(error => console.error('Error fetching cards:', error));
+
+
     return (
         <div>
             <MainBanner/>
