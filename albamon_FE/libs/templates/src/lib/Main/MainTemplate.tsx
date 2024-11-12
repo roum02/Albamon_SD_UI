3ddc8b2d3dff067a4a9ef77c4bc9756cbd383063
이로움 <JK1234~~>
2024-11-12 17:29:43 +0900

    Add SSR test [feature/react-query]

--- albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
@@ -1,5 +1,6 @@
 import { MainBanner } from '@components/lib';
 import { gql, useQuery } from "@apollo/client";
+import { client } from '@apollo-client/client'
 
 
 interface NextRequestInit extends RequestInit {
@@ -31,10 +32,10 @@ const GET_CARDS = gql`
 
 
 export const MainTemplate = () => {
+    // client version
     const { loading, error, data } = useQuery(GET_CARDS);
 
     console.log(data)
-
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
@@ -45,3 +46,25 @@ export const MainTemplate = () => {
     </div>
   );
 };
+
+// SSR version
+// export const getServerSideProps = async () => {
+//     try{
+//         const {data} = await client.query({
+//             query: GET_CARDS
+//         });
+//
+//         return {
+//             props: {
+//                 cards: data.getCards.collection,
+//             }
+//         };
+//     } catch (error){
+//         return {
+//             props: {
+//                 cards: [],
+//                 error: "Failed to fetch data"
+//             }
+//         }
+//     }
+// }
