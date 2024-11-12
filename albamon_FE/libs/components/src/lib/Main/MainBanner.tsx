204f7d31066e94dbec0c9bf4948e9190e2bd745f
이로움 <JK1234~~>
2024-11-12 20:25:24 +0900

    Add banner swiper [feature/react-query]

--- albamon_FE/libs/components/src/lib/Main/MainBanner.tsx
+++ albamon_FE/libs/components/src/lib/Main/MainBanner.tsx
@@ -1,3 +1,76 @@
-export const MainBanner = () => {
-  return <img src="/" alt="test" />;
+import {ReactNode} from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import classNames from 'classnames';
+
+interface MainBannerProps {
+  promotionBannerData: PromotionBannerData;
+}
+
+interface PromotionBannerData {
+  promotionBannerCount: number;
+  promotionBanners: PromotionBannerBffResponse[];
+}
+
+interface PromotionBannerBffResponse{
+  alternateTitle: string;
+  appMenuNo: number;
+  bannerBackGroundColor: string;
+  bannerImageUrl: string;
+  bannerLinkUrl: string;
+  bannerNo: number;
+}
+
+interface BannerItem{
+  src: string;
+  alt?: string;
+  linkUrl?: string;
+  textContent?: ReactNode;
+  isAbsolute?: boolean;
+  bannerGroupNo?: number;
+  backgroundColor?: string;
+}
+
+export const MainBanner = ({ promotionBannerData }: MainBannerProps) => {
+  const imageSlide = promotionBannerData.promotionBanners || [];
+
+  console.log(imageSlide)
+
+  return (
+      <div className={classNames('full-width', 'image-banner')}>
+          <Swiper
+              modules={[Pagination, Autoplay]}
+              pagination={{clickable: true}}
+              autoplay={{delay: 3000}}
+              loop={true}
+              spaceBetween={50}
+              slidesPerView={1}
+          >
+              {imageSlide.map((item, index) => (
+                  <SwiperSlide key={index}>
+                      {renderContent({src: item.bannerImageUrl, alt: item.alternateTitle})}
+                  </SwiperSlide>
+              ))}
+          </Swiper>
+      </div>
+  );
 };
+
+
+const renderContent = ({src, alt = ''}: BannerItem) => (
+    <div className={'image-banner__box'}>
+      {src && (
+          <img
+              //src={`${process.env['NEXT_PUBLIC_IMAGE_DEV']}${src}`}
+              src={`https:${src}`}
+              className={'image-banner__img'}
+              alt={alt}
+              width={1080}
+              height={588}
+          />
+      )}
+    </div>
+);
--- albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Main/MainTemplate.tsx
@@ -1,5 +1,5 @@
 import { MainBanner } from '@components/lib';
-import { gql, useQuery } from "@apollo/client";
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
 import { client } from '@apollo-client/client'
 import { NetworkStatus } from '@apollo/client';
 
@@ -31,23 +31,43 @@ const GET_CARDS = gql`
     }
 `;
 
+const GET_PROMOTION_BANNER = gql`
+query GetPromotionBanners{
+    getPromotionBanners {
+        promotionBannerCount
+        promotionBanners {
+            bannerImageUrl
+            bannerTitle
+            alternateTitle
+            appMenuNo
+            bannerBackGroundColor
+            bannerLinkUrl
+            bannerNo
+        }
+    }
+}
+`
+
+
 
 export const MainTemplate = () => {
     // client version
-    const { loading, error, data, networkStatus, refetch } = useQuery(GET_CARDS, {
+    const [getCards, { loading: loadingCards, error: errorCards, data: dataCards, networkStatus: networkStatusCards, refetch: refetchCards }] = useLazyQuery(GET_CARDS, {
         //pollInterval: 500,
         notifyOnNetworkStatusChange: true,
     });
 
-    console.log(data)
-    if (networkStatus === NetworkStatus.refetch) return <p>Refetching!</p>;
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
+    const { loading: loadingPromotion, error: errorPromotion, data: dataPromotion, networkStatus: networkStatusPromotion, refetch: refetchPromotion } = useQuery(GET_PROMOTION_BANNER, {})
+
+    if (networkStatusCards === NetworkStatus.refetch) return <p>Refetching!</p>;
+    if (loadingCards || loadingPromotion) return <p>Loading...</p>;
+    if (errorCards || errorPromotion) return <p>Error : {errorCards.message}</p>;
+
 
   return (
       <div>
-          <MainBanner/>
-          <button onClick={() => refetch()}>
+          <MainBanner promotionBannerData={dataPromotion.getPromotionBanners}/>
+          <button onClick={() => refetchCards()}>
               Refetch
           </button>
           <h1>main card list</h1>
