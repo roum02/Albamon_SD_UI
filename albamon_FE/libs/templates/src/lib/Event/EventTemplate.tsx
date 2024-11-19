a1e000bf13f652c666c2f31a5b43ebd8f8d767c4
이로움 <JK1234~~>
2024-11-19 19:48:16 +0900

    Add Event RenderComponent [feature/event-template]

--- albamon_FE/libs/templates/src/lib/Event/EventTemplate.tsx
+++ albamon_FE/libs/templates/src/lib/Event/EventTemplate.tsx
@@ -14,5 +14,114 @@ export const EventTemplate = () => {
   if (loading) return <p>loading...</p>;
   if (error) return <p>Error : {error?.message} </p>;
 
-  return <h1>event header</h1>;
+  return (
+    <>
+      {eventList.getEventPageComponents.components.map((item, index) => (
+        <RenderComponent key={`${item.type}_${index}`} {...item} />
+      ))}
+    </>
+  );
+};
+
+const Title = ({ text, fontSize, fontWeight, textAlign, color }) => (
+  <h1
+    style={{
+      fontSize,
+      fontWeight,
+      textAlign,
+      color,
+    }}
+  >
+    {text}
+  </h1>
+);
+
+const ImageWithButton = ({
+  imageUrl,
+  buttonText,
+  buttonBackgroundColor,
+  buttonTextColor,
+  children,
+}) => (
+  <div>
+    <img src={imageUrl} alt="example" />
+    <button
+      style={{
+        backgroundColor: buttonBackgroundColor,
+        color: buttonTextColor,
+      }}
+      onClick={() =>
+        children?.find((child) => child.type === 'BUTTON')?.onClick?.()
+      }
+    >
+      {buttonText}
+    </button>
+  </div>
+);
+
+const Image = ({
+  imageUrl,
+  backgroundColor,
+  paddingTop,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+}) => (
+  <div
+    style={{
+      backgroundColor,
+      paddingTop,
+      paddingBottom,
+      paddingLeft,
+      paddingRight,
+    }}
+  >
+    <img src={imageUrl} alt="example" />
+  </div>
+);
+
+const FloatingButton = ({
+  text,
+  backgroundColor,
+  textColor,
+  position,
+  bottom,
+}) => (
+  <button
+    style={{
+      position,
+      bottom,
+      backgroundColor,
+      color: textColor,
+    }}
+  >
+    {text}
+  </button>
+);
+
+const Footer = ({ text, fontSize, fontWeight, textAlign, color }) => (
+  <footer
+    style={{
+      fontSize,
+      fontWeight,
+      textAlign,
+      color,
+    }}
+  >
+    {text}
+  </footer>
+);
+
+const MAPPED_COMPONENTS = {
+  TITLE: Title,
+  IMAGE_WITH_BUTTON: ImageWithButton,
+  IMAGE: Image,
+  FLOATING_BUTTON: FloatingButton,
+  FOOTER: Footer,
+};
+
+const RenderComponent = (data) => {
+  //console.log(data);
+  const Component = MAPPED_COMPONENTS[data.type];
+  return Component && <Component {...data} />;
 };
