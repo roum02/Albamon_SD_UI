4d9fa938f77555df285747332da577c078e15030
gyulrangdev <gyulrangdev@gmail.com>
2024-11-26 19:57:10 +0900

    Feat: 타이틀 추가

--- albamon_FE/libs/components/src/lib/Event/Title.tsx
+++ albamon_FE/libs/components/src/lib/Event/Title.tsx
@@ -1,12 +1,37 @@
+import { IoMdArrowBack } from 'react-icons/io';
+import { RxHamburgerMenu } from 'react-icons/rx';
+
 export const Title = ({ text, fontSize, fontWeight, textAlign, color }) => (
-  <h1
+  <div
     style={{
-      fontSize,
-      fontWeight,
-      textAlign,
-      color,
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      height: '53px',
+      justifyContent: 'space-between',
     }}
   >
-    {text}
-  </h1>
+    <IoMdArrowBack
+      size={24}
+      style={{ marginLeft: 20, marginRight: 15, flexShrink: 0 }}
+      onClick={() => alert('back clicked')}
+    />
+    <div style={{ width: '100%' }}>
+      <h1
+        style={{
+          fontSize,
+          fontWeight,
+          textAlign,
+          color,
+        }}
+      >
+        {text}
+      </h1>
+    </div>
+    <RxHamburgerMenu
+      size={24}
+      style={{ marginRight: 20, marginLeft: 15, flexShrink: 0 }}
+      onClick={() => alert('hamburger clicked')}
+    />
+  </div>
 );
