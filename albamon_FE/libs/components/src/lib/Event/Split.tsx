7fe3f30b1afa8dc1554c423478fdf9159c6a67e5
gyulrangdev <gyulrangdev@gmail.com>
2024-11-26 21:21:25 +0900

    Feat: Split 추가

--- albamon_FE/libs/components/src/lib/Event/Split.tsx
+++ albamon_FE/libs/components/src/lib/Event/Split.tsx
@@ -1,3 +1,11 @@
 export const Split = ({ ...props }) => {
-  return <div style={{ ...props }}>split</div>;
+  return (
+    <div
+      style={{
+        width: '100%',
+        borderTop: '1px dashed #DDDDDD',
+        ...props,
+      }}
+    />
+  );
 };
